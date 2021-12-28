fae3460e49d1e1b363584d565ffa501bc0a6eb0f
Arseniy Aphonin <ars.20013@yandex.ru>
2021-12-28 08:39:30 +0300

    add: rename some panels

--- adminForm.h
+++ adminForm.h
@@ -58,7 +58,7 @@ namespace bshop {
 
 
 	private: System::Windows::Forms::TabPage^ addUserTab;
-	private: System::Windows::Forms::Panel^ addUserPanel;
+	private: System::Windows::Forms::Panel^ userRegistrationPanel;
 	private: System::Windows::Forms::MaskedTextBox^ addUserPhonenumberTB;
 	private: System::Windows::Forms::MaskedTextBox^ addUserDateTB;
 	private: System::Windows::Forms::GroupBox^ addUserSexGP;
@@ -201,7 +201,7 @@ private: System::Windows::Forms::DataGridViewTextBoxColumn^ serviceInOrder;
 			this->orderStatus = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->serviceInOrder = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->addUserTab = (gcnew System::Windows::Forms::TabPage());
-			this->addUserPanel = (gcnew System::Windows::Forms::Panel());
+			this->userRegistrationPanel = (gcnew System::Windows::Forms::Panel());
 			this->addUserPhonenumberTB = (gcnew System::Windows::Forms::MaskedTextBox());
 			this->addUserDateTB = (gcnew System::Windows::Forms::MaskedTextBox());
 			this->addUserSexGP = (gcnew System::Windows::Forms::GroupBox());
@@ -299,7 +299,7 @@ private: System::Windows::Forms::DataGridViewTextBoxColumn^ serviceInOrder;
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->servicesDataGrid))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->ordersDataGrid))->BeginInit();
 			this->addUserTab->SuspendLayout();
-			this->addUserPanel->SuspendLayout();
+			this->userRegistrationPanel->SuspendLayout();
 			this->addUserSexGP->SuspendLayout();
 			this->specialityPanel->SuspendLayout();
 			this->changeUserTab->SuspendLayout();
@@ -357,7 +357,7 @@ private: System::Windows::Forms::DataGridViewTextBoxColumn^ serviceInOrder;
 			this->mainTabSekector->Controls->Add(this->addServiceTab);
 			this->mainTabSekector->Controls->Add(this->changeServiceTab);
 			this->mainTabSekector->Controls->Add(this->showReportTab);
-			this->mainTabSekector->Location = System::Drawing::Point(301, 3);
+			this->mainTabSekector->Location = System::Drawing::Point(302, 3);
 			this->mainTabSekector->Name = L"mainTabSekector";
 			this->mainTabSekector->SelectedIndex = 0;
 			this->mainTabSekector->Size = System::Drawing::Size(583, 712);
@@ -554,7 +554,7 @@ private: System::Windows::Forms::DataGridViewTextBoxColumn^ serviceInOrder;
 			// addUserTab
 			// 
 			this->addUserTab->BackColor = System::Drawing::Color::Gainsboro;
-			this->addUserTab->Controls->Add(this->addUserPanel);
+			this->addUserTab->Controls->Add(this->userRegistrationPanel);
 			this->addUserTab->Location = System::Drawing::Point(4, 22);
 			this->addUserTab->Name = L"addUserTab";
 			this->addUserTab->Padding = System::Windows::Forms::Padding(3);
@@ -562,29 +562,29 @@ private: System::Windows::Forms::DataGridViewTextBoxColumn^ serviceInOrder;
 			this->addUserTab->TabIndex = 1;
 			this->addUserTab->Text = L"Добавить пользователя";
 			// 
-			// addUserPanel
+			// userRegistrationPanel
 			// 
-			this->addUserPanel->Controls->Add(this->addUserPhonenumberTB);
-			this->addUserPanel->Controls->Add(this->addUserDateTB);
-			this->addUserPanel->Controls->Add(this->addUserSexGP);
-			this->addUserPanel->Controls->Add(this->specialityPanel);
-			this->addUserPanel->Controls->Add(this->addUserTypeCB);
-			this->addUserPanel->Controls->Add(this->addUserTypeLbl);
-			this->addUserPanel->Controls->Add(this->addUserFormBtn);
-			this->addUserPanel->Controls->Add(this->addPassLbl);
-			this->addUserPanel->Controls->Add(this->addEmailLbl);
-			this->addUserPanel->Controls->Add(this->addPhonenumberLbl);
-			this->addUserPanel->Controls->Add(this->addDateLbl);
-			this->addUserPanel->Controls->Add(addNameLbl);
-			this->addUserPanel->Controls->Add(this->addUserPasswordTB);
-			this->addUserPanel->Controls->Add(this->addUserMailTB);
-			this->addUserPanel->Controls->Add(this->addUserNameTB);
-			this->addUserPanel->Controls->Add(this->addUser);
-			this->addUserPanel->Font = (gcnew System::Drawing::Font(L"Yu Gothic UI Semibold", 9.25F, System::Drawing::FontStyle::Bold));
-			this->addUserPanel->Location = System::Drawing::Point(6, 5);
-			this->addUserPanel->Name = L"addUserPanel";
-			this->addUserPanel->Size = System::Drawing::Size(563, 675);
-			this->addUserPanel->TabIndex = 26;
+			this->userRegistrationPanel->Controls->Add(this->addUserPhonenumberTB);
+			this->userRegistrationPanel->Controls->Add(this->addUserDateTB);
+			this->userRegistrationPanel->Controls->Add(this->addUserSexGP);
+			this->userRegistrationPanel->Controls->Add(this->specialityPanel);
+			this->userRegistrationPanel->Controls->Add(this->addUserTypeCB);
+			this->userRegistrationPanel->Controls->Add(this->addUserTypeLbl);
+			this->userRegistrationPanel->Controls->Add(this->addUserFormBtn);
+			this->userRegistrationPanel->Controls->Add(this->addPassLbl);
+			this->userRegistrationPanel->Controls->Add(this->addEmailLbl);
+			this->userRegistrationPanel->Controls->Add(this->addPhonenumberLbl);
+			this->userRegistrationPanel->Controls->Add(this->addDateLbl);
+			this->userRegistrationPanel->Controls->Add(addNameLbl);
+			this->userRegistrationPanel->Controls->Add(this->addUserPasswordTB);
+			this->userRegistrationPanel->Controls->Add(this->addUserMailTB);
+			this->userRegistrationPanel->Controls->Add(this->addUserNameTB);
+			this->userRegistrationPanel->Controls->Add(this->addUser);
+			this->userRegistrationPanel->Font = (gcnew System::Drawing::Font(L"Yu Gothic UI Semibold", 9.25F, System::Drawing::FontStyle::Bold));
+			this->userRegistrationPanel->Location = System::Drawing::Point(6, 5);
+			this->userRegistrationPanel->Name = L"userRegistrationPanel";
+			this->userRegistrationPanel->Size = System::Drawing::Size(563, 675);
+			this->userRegistrationPanel->TabIndex = 26;
 			// 
 			// addUserPhonenumberTB
 			// 
@@ -1496,8 +1496,8 @@ private: System::Windows::Forms::DataGridViewTextBoxColumn^ serviceInOrder;
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->servicesDataGrid))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->ordersDataGrid))->EndInit();
 			this->addUserTab->ResumeLayout(false);
-			this->addUserPanel->ResumeLayout(false);
-			this->addUserPanel->PerformLayout();
+			this->userRegistrationPanel->ResumeLayout(false);
+			this->userRegistrationPanel->PerformLayout();
 			this->addUserSexGP->ResumeLayout(false);
 			this->addUserSexGP->PerformLayout();
 			this->specialityPanel->ResumeLayout(false);
